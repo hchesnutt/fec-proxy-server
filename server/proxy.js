5752a2498bc9193abb7683737c8839bffcf54fc2
hchesnutt <t.henry.chesnutt@gmail.com>
2018-04-24 16:06:36 -0700

    add revews and similar listings

---
+++ server/proxy.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors')
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static((path.join(__dirname, '../client/dist'))));
+app.use((req, res, next) => {
+  console.log(`Serving ${req.method} request to ${req.url}`);
+  next();
+});
+
+app.listen(PORT, () => console.log(`Proxy server listening at http://localhost:${PORT}`));
